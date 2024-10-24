9e51ac8bf6422ead5732b2f3e4b67f5434d3083e
Eduardo Morôni <eduardomoroni@gmail.com>
2024-10-24 22:24:16 +0200

    initial commit

---
+++ src/app/cards/[name]/page.tsx
@@ -0,0 +1,44 @@
+interface Card {
+    id: string
+    title: string
+    content: string
+}
+
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 86400 // 24 hours
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
+
+const  url = 'https://play.lorcanito.com/api/sets/001';
+
+export async function generateStaticParams() {
+
+    return [...Array(204).keys()].map((i) => {
+        return {
+            params: {
+                id: i.toString()
+            }
+        }
+    })
+}
+
+export default async function Page({ params }: { params: { name: string } }) {
+    const id = (await params).name;
+
+    const card: Card = await fetch(
+        url
+    ).then((res) => res.json()).then((data) => {
+        return data.cards.find((card: unknown) => String(card.number) === String(id));
+    })
+
+    return (
+        <main>
+            <h1>{id}</h1>
+            <p>{JSON.stringify(card)}</p>
+        </main>
+    )
+}
