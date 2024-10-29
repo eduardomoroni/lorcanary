c285972874f93dfdca112a99ed7b9f4c6074ff9b
Eduardo Morôni <eduardomoroni@gmail.com>
2024-10-29 13:28:08 +0100

    add params

--- src/app/cards/[setOrName]/[number]/page.tsx
+++ src/app/cards/[setOrName]/[number]/page.tsx
@@ -1,10 +1,16 @@
 import CardPage from "@/spaces/cards/CardPage";
-export {
-  generateStaticParams,
-  dynamic,
-  dynamicParams,
-  revalidate,
-} from "@/spaces/cards/CardPage";
+export { generateStaticParams } from "@/spaces/cards/CardPage";
 export { generateMetadata } from "@/spaces/cards/CardPageMetadata";
+
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 86400; // 24 hours
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
+export const dynamic = "force-static";
 
 export default CardPage;
