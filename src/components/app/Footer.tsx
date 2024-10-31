c5851ba5dec9ed07fa5923a4a1f82dd72c408903
Eduardo Morôni <eduardomoroni@gmail.com>
2024-10-31 12:30:45 +0100

    discord verify

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -13,9 +13,10 @@ export default async function Footer() {
     // </footer>
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center m500:text-sm z-30 bg-main50 px-5 py-5 text-center font-base dark:bg-secondaryBlack">
       This website uses trademarks and/or copyrights associated with Disney
-      Lorcana TCG, used under
+      Lorcana TCG, used under{" "}
       <a
         target="_blank"
+        className="underline"
         href="https://cdn.ravensburger.com/lorcana/community-code-en"
       >
         Ravensburger’s Community Code Policy
@@ -23,8 +24,12 @@ export default async function Footer() {
       . We are expressly prohibited from charging you to use or access this
       content. This website is not published, endorsed, or specifically approved
       by Disney or Ravensburger. For more information about Disney Lorcana TCG,
-      visit{" "}
-      <a target="_blank" href="https://www.disneylorcana.com/en-US/">
+      visit
+      <a
+        target="_blank"
+        className="underline"
+        href="https://www.disneylorcana.com/en-US/"
+      >
         https://www.disneylorcana.com
       </a>
       .
