6cb09e99fbe77f099924e18a1774419c7d33c6a0
Eduardo Morôni <eduardomoroni@gmail.com>
2024-12-06 07:55:49 +0100

    Revert "add cards"
    This reverts commit f5f53286d9701c44482240952f36aa4974865ec5.

--- src/data/lorcanitoCards.ts
+++ src/data/lorcanitoCards.ts
@@ -1,20 +1,35 @@
+import type { LorcanitoCard } from "@/shared/types/lorcanito";
 import { cardNameToUrlSafeString } from "@/shared/strings";
-import { allCards } from "@/db/cards/cards";
 
-export async function getCardBySetAndNumber(set: string, number: string) {
-  return getAllCards().then((data) =>
-    data.find((card) => Number(card.number) === Number(number)),
-  );
+export async function getCardBySetAndNumber(
+  set: string,
+  number: string,
+): Promise<LorcanitoCard | undefined> {
+  return fetch(`https://play.lorcanito.com/api/sets/${set}`, {
+    cache: "force-cache",
+  })
+    .then((res) => res.json())
+    .then((data) =>
+      data.cards.find(
+        (card: LorcanitoCard) => Number(card.number) === Number(number),
+      ),
+    );
 }
 
-export async function getAllCards() {
-  return allCards;
+export async function getAllCards(): Promise<LorcanitoCard[]> {
+  const res = await fetch(`https://play.lorcanito.com/api/sets/all`, {
+    cache: "force-cache",
+  });
+  return (await res.json()).cards || [];
 }
 
-export async function getCardByName(name: string, title?: string) {
+export async function getCardByName(
+  name: string,
+  title?: string,
+): Promise<LorcanitoCard | undefined> {
   return getAllCards().then((data) =>
     data.find(
-      (card) =>
+      (card: LorcanitoCard) =>
         cardNameToUrlSafeString(card.name, card.title) === name ||
         cardNameToUrlSafeString(card.name, card.title) ===
           cardNameToUrlSafeString(name, title),
@@ -24,6 +39,8 @@ export async function getCardByName(name: string, title?: string) {
 
 export async function allLorcanitoCardNames(): Promise<string[]> {
   return getAllCards().then((data) =>
-    data.map((card) => cardNameToUrlSafeString(card.name, card.title)),
+    data.map((card: LorcanitoCard) =>
+      cardNameToUrlSafeString(card.name, card.title),
+    ),
   );
 }
